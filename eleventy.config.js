afb19ce5d5a614407863c2fa4731aa4b1e182c18
bloomingbridges <brueckner.florian@googlemail.com>
2024-10-17 17:15:41 +0200

    Setup and placeholder images.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,15 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.ignores.add("*.md");
-
+	// eleventyConfig.ignores.add("*.md");
+	eleventyConfig.addPassthroughCopy({ public: "./" });
 	eleventyConfig.addPlugin(pluginWebc);
-
 	eleventyConfig.setServerOptions({
 		domDiff: false
 	});
+
+	return {
+		markdownTemplateEngine: "webc"
+	}
+
 };
---
+++ index.md
@@ -0,0 +1,19 @@
+---
+layout: layout.webc
+---
+# Eleventy WebC Album Demonstration
+
+This is only a test
+of the emergency broadcast system
+
+<album>
+![A](/a.png)
+![B](/b.png)
+![C](/c.png)
+</album>
+
+This is the product of
+hysterical mass confusion.
+
+![D](/d.png)
+![E](/e.png)
